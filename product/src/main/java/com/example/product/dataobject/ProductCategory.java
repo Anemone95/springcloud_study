ea27f09fb887e43dfdbac924b226952160b124cb
Anemone95 <x565178035@126.com>
2019-06-06 10:54:23 +0800

    19/09/06 init commit

---
+++ product/src/main/java/com/example/product/dataobject/ProductCategory.java
@@ -0,0 +1,21 @@
+package com.example.product.dataobject;
+
+import lombok.Data;
+import lombok.Generated;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Data
+@Entity
+public class ProductCategory {
+    @Id
+    @GeneratedValue
+    private Integer categoryId;
+    private String categoryName;
+    private Integer categoryType;
+    private Date createTime;
+    private Date updateTime;
+}
---
+++ product/src/main/java/com/example/product/dataobject/ProductInfo.java
@@ -0,0 +1,27 @@
+package com.example.product.dataobject;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@Entity
+public class ProductInfo {
+
+    @Id
+    private String productId;
+    private String productName;
+    // 单价
+    private BigDecimal productPrice;
+    private Integer productStock;
+    private String productDescription;
+    private String productIcon;
+    private Integer productStatus;
+    private Integer categoryType;
+    private Date createTime;
+    private Date updateTime;
+}
