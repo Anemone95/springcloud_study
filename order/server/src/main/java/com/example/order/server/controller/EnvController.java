5533004b85a71424929cd7d48b6122645bce66c0
Anemone95 <x565178035@126.com>
2019-06-11 09:50:45 +0800

    19/06/11 add ambp failed

--- order/server/src/main/java/com/example/order/server/controller/EnvController.java
+++ order/server/src/main/java/com/example/order/server/controller/EnvController.java
@@ -1,14 +1,16 @@
 package com.example.order.server.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/env")
+@RefreshScope
 public class EnvController {
-    @Value("${spring.application.name}")
+    @Value("${env}")
     private String env;
 
     @GetMapping("/print")
